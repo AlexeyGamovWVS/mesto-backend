8417ccf23c1d6d6cea8fe715b521fb6707e92358
Alexey Gamov <lexa2009xxxx@yandex.ru>
2024-03-06 20:58:17 +0300

    feat: создать модели для карточки и пользователя

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const DEFAULTS = {
 const { PORT = DEFAULTS.PORT } = process.env;
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect("mongodb://localhost:27017/mestodb");
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
